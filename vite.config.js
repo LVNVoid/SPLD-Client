20d3e55de5be30cf8a486130e824dfd5e594eef8
OjekPantai <elvien.purnawan01@gmail.com>
2025-06-13 18:10:03 +0700

    fix: add Tailwind CSS plugin to Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig, loadEnv } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
 
   return {
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
     server: {
       proxy: {
         "/api": {
