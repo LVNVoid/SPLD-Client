a5e5a0f1da978c0bda1ee6f7cb6e8ca33389b735
OjekPantai <elvien.purnawan01@gmail.com>
2025-06-13 18:27:19 +0700

    fix: update API URLs in axios configuration and Vite proxy settings

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,15 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-
   return {
     plugins: [react(), tailwindcss()],
     server: {
       proxy: {
         "/api": {
-          target: env.VITE_API_URL,
+          target: "https://spld-server.vercel.app",
           changeOrigin: true,
         },
       },
