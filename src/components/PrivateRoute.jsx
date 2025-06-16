92232d7bfb4dd52038c60e67ab7c2b5c4f3f2523
OjekPantai <elvien.purnawan01@gmail.com>
2025-06-16 10:16:39 +0700

    refactor: simplify PrivateRoute by removing token verification and loading state; streamline user authentication flow

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,40 +1,15 @@
-import { login } from "@/features/userSlice";
-import api from "@/lib/axios";
-import { useEffect, useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 
 const PrivateRoute = ({ allowedRoles }) => {
-  const user = useSelector((state) => state.userState.user);
-  const [loading, setLoading] = useState(true);
-  const [authUser, setAuthUser] = useState(null);
+  const { user } = useSelector((state) => state.userState);
   const location = useLocation();
-  const dispatch = useDispatch();
 
-  useEffect(() => {
-    const checkAuth = async () => {
-      if (!user) {
-        const fetchedUser = await verifyToken();
-        if (fetchedUser) {
-          dispatch(login(fetchedUser));
-          setAuthUser(fetchedUser);
-        }
-      } else {
-        setAuthUser(user);
-      }
-      setLoading(false);
-    };
-
-    checkAuth();
-  }, [user, dispatch]);
-
-  if (loading) return <div className="p-4">Loading...</div>;
-
-  if (!authUser) {
+  if (!user) {
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
 
-  if (allowedRoles && !allowedRoles.includes(authUser.role)) {
+  if (allowedRoles && !allowedRoles.includes(user.role)) {
     return <Navigate to="/unauthorized" replace />;
   }
 
@@ -42,17 +17,3 @@ const PrivateRoute = ({ allowedRoles }) => {
 };
 
 export default PrivateRoute;
-
-async function verifyToken() {
-  try {
-    const res = await api.get("/auth/validate");
-
-    if (!res.ok) return null;
-
-    const data = await res.json();
-    return data.user;
-  } catch (err) {
-    console.error(err);
-    return null;
-  }
-}
