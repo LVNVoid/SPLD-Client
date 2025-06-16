16d85390019a7c2dc514869da365753680bb123d
OjekPantai <elvien.purnawan01@gmail.com>
2025-06-16 08:23:34 +0700

    feat: implement session validation in PrivateRoute; add loading spinner and user data loading state

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,12 +1,54 @@
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
+import api from "@/lib/axios"; // Sesuaikan dengan path Anda
+import Spinner from "./ui/spinner";
 
 const PrivateRoute = ({ allowedRoles }) => {
   const { user } = useSelector((state) => state.userState);
   const location = useLocation();
+  const [isValidating, setIsValidating] = useState(true);
+  const [isValidSession, setIsValidSession] = useState(false);
+
+  useEffect(() => {
+    const validateSession = async () => {
+      try {
+        const response = await api.get("/auth/validate", {
+          withCredentials: true,
+        });
+        setIsValidSession(response.data.success);
+      } catch (error) {
+        console.error("Session validation failed:", error);
+        setIsValidSession(false);
+      } finally {
+        setIsValidating(false);
+      }
+    };
+
+    validateSession();
+  }, []);
+
+  if (isValidating) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <p>
+          <Spinner className="animate-spin" size={20} />
+          Memverifikasi sesi...
+        </p>
+      </div>
+    );
+  }
+
+  if (!isValidSession) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
 
   if (!user) {
-    return <Navigate to="/login" state={{ from: location }} replace />;
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <p>Memuat data pengguna...</p>
+      </div>
+    );
   }
 
   if (allowedRoles && !allowedRoles.includes(user.role)) {
