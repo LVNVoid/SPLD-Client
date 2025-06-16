a0ee3ea3c05b6e8a858f50fff0f94b7cdebc9c68
OjekPantai <elvien.purnawan01@gmail.com>
2025-06-16 13:13:36 +0700

    refactor: remove unused authentication logic and simplify PrivateRoute component

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,39 +1,9 @@
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { login, logout } from "@/features/userSlice"; // pastikan ini sesuai pathmu
-import api from "@/lib/axios";
 
 const PrivateRoute = ({ allowedRoles }) => {
   const { user } = useSelector((state) => state.userState);
-  const dispatch = useDispatch();
   const location = useLocation();
-  const [checkingAuth, setCheckingAuth] = useState(true);
-
-  useEffect(() => {
-    const checkAuth = async () => {
-      try {
-        const res = await api.get("/auth/me");
-        if (!res.ok) throw new Error("Unauthorized");
-        const data = await res.json();
-        dispatch(login(data.user));
-      } catch (err) {
-        dispatch(logout());
-      } finally {
-        setCheckingAuth(false);
-      }
-    };
-
-    if (!user) {
-      checkAuth();
-    } else {
-      setCheckingAuth(false);
-    }
-  }, [user, dispatch]);
-
-  if (checkingAuth) {
-    return <div>Loading...</div>;
-  }
 
   if (!user) {
     return <Navigate to="/login" state={{ from: location }} replace />;
